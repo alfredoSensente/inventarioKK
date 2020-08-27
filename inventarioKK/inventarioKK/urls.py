b464526baa413a68b483b24ab89f6ede21bfbfa9
alfredo <ajsnsn93@gmail.com>
2020-08-27 17:39:15 -0600

    se agregaron las apps de empleado y equipo

--- inventarioKK/inventarioKK/urls.py
+++ inventarioKK/inventarioKK/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('empleado/', include('empleado.urls')),
+    path('equipo/', include('equipo.urls')),
 ]
