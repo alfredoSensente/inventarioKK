fcb7b56b663c0cb2fb6adccf2baafc6156f2f52a
ToshiroEstrada <le10001@ues.edu.sv>
2020-08-27 14:47:48 -0600

    el Crustaceo Cascarudo abrio sus puertas (se creo la bd)

--- inventarioKK/inventarioKK/settings.py
+++ inventarioKK/inventarioKK/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'inventarioKK.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crustaceocascarudo',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
