cc9f314a961ce4d6da789ef42351128d560985ac
alfredo <ajsnsn93@gmail.com>
2020-09-01 10:19:10 -0600

    Se crearon los primero modelos de la app equipo y su primera migracion

--- inventarioKK/equipo/models.py
+++ inventarioKK/equipo/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Marca(models.Model):
+    id_marca = models.AutoField(primary_key=True)
+    nombre_marca = models.CharField(max_length=45)
+
+    class Meta:
+        managed = True
+        db_table = 'marca'
+
+class TipoEquipo(models.Model):
+    id_tipo_equipo = models.AutoField(primary_key=True)
+    nombre_tipo_equipo = models.CharField(max_length=45)
+
+    class Meta:
+        managed = True
+        db_table = 'tipo_equipo'
+
+class Equipo(models.Model):
+    id_equipo = models.AutoField(primary_key=True)
+    id_tipo_equipo = models.ForeignKey(TipoEquipo, models.DO_NOTHING, db_column='id_tipo_equipo')
+
+
+    class Meta:
+        managed = True
+        db_table = 'equipo'
+        unique_together = (('id_equipo', 'id_tipo_equipo',),)
