7f706e8ac5046c87fe5690f73ae2a799179131a2
ToshiroEstrada <le10001@ues.edu.sv>
2020-09-04 10:28:54 -0600

    hice todos los modelos del area de equipo

--- inventarioKK/equipo/models.py
+++ inventarioKK/equipo/models.py
@@ -17,12 +17,48 @@ class TipoEquipo(models.Model):
         managed = True
         db_table = 'tipo_equipo'
 
+
+class Edificio(models.Model):
+    id_edificio = models.AutoField(primary_key=True)
+    nombre_edificio = models.CharField(max_length=45)
+
+    class Meta:
+        managed = True
+        db_table = 'edificio'
+
+
+class Ubicacion(models.Model):
+    id_ubicacion = models.AutoField(primary_key=True)
+    nombre_ubicacion = models.CharField(max_length=45)
+    id_edificio = models.ForeignKey(Edificio, on_delete=models.CASCADE, db_column='id_edificio')
+
+
+     class Meta:
+        managed = True
+        db_table = 'ubicacion'
+        unique_together = (('id_ubicacion', 'id_edificio'),)
+
+
+class EstadoEquipo(models.Model):
+    id_estado_equipo = models.AutoField(primary_key=True)
+    nombre_estado_equipo = models.CharField(max_length=45)
+
+        class Meta:
+        managed = True
+        db_table = 'estado_equipo'
+
+
 class Equipo(models.Model):
     id_equipo = models.AutoField(primary_key=True)
-    id_tipo_equipo = models.ForeignKey(TipoEquipo, models.DO_NOTHING, db_column='id_tipo_equipo')
-
+    id_tipo_equipo = models.ForeignKey(TipoEquipo, on_delete=models.CASCADE, db_column='id_tipo_equipo')
+    id_marca = models.ForeignKey(Marca, on_delete=models.CASCADE, db_column='id_marca')
+    id_estado_equipo = models.ForeignKey(EstadoEquipo, on_delete=models.CASCADE, db_column='id_estado_equipo')
+    id_ubicacion = models.ForeignKey(Ubicacion, on_delete=models.CASCADE, db_column='id_ubicacion')
+    descripcion = models.TextField(blank=True, null=True)
 
     class Meta:
         managed = True
         db_table = 'equipo'
-        unique_together = (('id_equipo', 'id_tipo_equipo',),)
+        unique_together = (('id_equipo', 'id_tipo_equipo','id_marca','id_estado_equipo','id_ubicacion'),)
+
+
