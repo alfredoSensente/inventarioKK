4426e56d9c63013fe342fc7f143c557b4a18d5b9
alfredo <ajsnsn93@gmail.com>
2020-09-06 11:44:00 -0600

    sitio admin puede utilizar los modelos de mantenimiento

--- inventarioKK/mantenimiento/admin.py
+++ inventarioKK/mantenimiento/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Mantenimiento
+from .models import TipoCorrectivo
+from .models import Correctivo
+from .models import MetodoPreventivo
+from .models import TipoPreventivo
+from .models import Preventivo
+from .models import Innovativo
 
 # Register your models here.
+admin.site.register(Mantenimiento)
+admin.site.register(TipoCorrectivo)
+admin.site.register(Correctivo)
+admin.site.register(MetodoPreventivo)
+admin.site.register(TipoPreventivo)
+admin.site.register(Preventivo)
+admin.site.register(Innovativo)
