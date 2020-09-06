f5b01b202ae00a042d4f95b129865eb1f8e0870a
ToshiroEstrada <le10001@ues.edu.sv>
2020-09-06 11:47:48 -0600

    sitio admin ya puede utilizar los modelos de Equipo

--- inventarioKK/equipo/admin.py
+++ inventarioKK/equipo/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Marca
+from .models import TipoEquipo
+from .models import Edificio
+from .models import Ubicacion
+from .models import EstadoEquipo
+from .models import Equipo
+
 
 # Register your models here.
+
+admin.site.register(Marca)
+admin.site.register(TipoEquipo)
+admin.site.register(Edificio)
+admin.site.register(Ubicacion)
+admin.site.register(EstadoEquipo)
+admin.site.register(Equipo)
+
