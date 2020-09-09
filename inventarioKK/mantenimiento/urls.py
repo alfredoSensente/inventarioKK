64d6d00a328ced481caa27a9ee0e2261b8486744
alfredo <ajsnsn93@gmail.com>
2020-09-09 10:24:27 -0600

    se creo el codigo inicial de layout_base y un ejemplo hola_mundo en la app de Mantenimiento

--- inventarioKK/mantenimiento/urls.py
+++ inventarioKK/mantenimiento/urls.py
@@ -1,4 +1,7 @@
+"""urls de mantenimiento"""
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.hola_mundo, name='hola_mundo')
 ]
--- inventarioKK/mantenimiento/views.py
+++ inventarioKK/mantenimiento/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def hola_mundo(request):
+    """Muestra la template de Hola Mundo"""
+    return render(request, 'mantenimiento/index.html')
