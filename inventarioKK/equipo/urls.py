f80d3df498b6922475ba24d08e9d7b6e23acea74
ToshiroEstrada <le10001@ues.edu.sv>
2020-09-10 11:39:09 -0600

    (Equipo/)se creo una tabla en el index de Equipo que muestra los registros de equipo

--- inventarioKK/equipo/urls.py
+++ inventarioKK/equipo/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.lista_de_equipos, name='lista_de_equipos')
 ]
--- inventarioKK/equipo/views.py
+++ inventarioKK/equipo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Equipo
 
 # Create your views here.
+def lista_de_equipos(request):
+    """contexto para la tabla equipo"""
+    lista_equipo = Equipo.objects.all()
+    context = {'lista_equipo': lista_equipo}
+    return render(request, 'equipo/index.html', context)
