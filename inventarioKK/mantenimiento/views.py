7a6216bb7a3304fa485b305c59c8c7e42ae081a4
mugen <ajsnsn93@gmail.com>
2020-09-19 21:47:13 -0600

    (mantenimiento/) Se aplicaron vistas genericas, se creo forms.py con un formulario para correctivo, se añadieron las vistas de nuevo_correctivo.html y borrar_mantenimiento.html

--- inventarioKK/mantenimiento/views.py
+++ inventarioKK/mantenimiento/views.py
@@ -1,19 +1,34 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Mantenimiento
+"""Vistas de Mantenimiento"""
+from django.views import generic
+from django.urls import reverse_lazy
+from .forms import CorrectivoForm
+from .models import Mantenimiento, Correctivo
 
 # Create your views here.
-def index(request):
-    """Muestra un listado de los mantenimientos existentes"""
-    listado_mantenimiento = Mantenimiento.objects.all()
-    context = {
-        'listado_mantenimiento' : listado_mantenimiento,
-    }
-    return render(request, 'mantenimiento/index.html', context)
+class Index(generic.ListView):
+    """Vista generica Index"""
+    # Matenimiento.objects.all()
+    template_name = 'mantenimiento/index.html'
+    context_object_name = 'listado_mantenimiento'
+    model = Mantenimiento
 
-def descripcion(request, id_mantenimiento):
-    """Muestra una descripcion del mantenimiento realizado a cierto equipo"""
-    descripcion_mantenimiento = get_object_or_404(Mantenimiento, pk=id_mantenimiento)
-    context = {
-        'descripcion_mantenimiento' : descripcion_mantenimiento
-    }
-    return render(request, 'mantenimiento/descripcion.html', context)
+class DescripcionMatenimiento(generic.DetailView):
+    """Vista generica de los matenimientos realizados a un equipo"""
+    # Mantenimiento.objects.get(pk=1)
+    model = Mantenimiento
+    template_name = 'mantenimiento/descripcion.html'
+    context_object_name = 'descripcion_mantenimiento'
+
+class CorretivoCreate(generic.CreateView):
+    """Formulario para agregar un nuevo mantenimiento correctivo"""
+    model = Correctivo
+    form_class = CorrectivoForm
+    template_name = 'mantenimiento/nuevo_correctivo.html'
+    success_url = reverse_lazy('mantenimiento:index')
+
+class MantenimientoDelete(generic.DeleteView):
+    """Vista para eleminar un registro de mantenimiento"""
+    model = Mantenimiento
+    context_object_name = 'mantenimiento_objeto'
+    template_name = 'mantenimiento/borrar_mantenimiento.html'
+    success_url = reverse_lazy('mantenimiento:index')
