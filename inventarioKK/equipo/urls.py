5e49eb509f175f3669390c45aef5e4dcbb17228e
ToshiroEstrada <le10001@ues.edu.sv>
2020-09-26 10:05:36 -0600

    se añadieron los archivos utils, la direccion url para la seccion de pdfs, y su respectiva view

--- inventarioKK/equipo/urls.py
+++ inventarioKK/equipo/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('nuevo_equipo/', views.EquipoCreate.as_view(), name='nuevo_equipo'),
     path('<int:pk>/', views.DescripcionEquipo.as_view(), name='des_equipo'),
     path('editar_equipo/<int:pk>/', views.EquipoUpdate.as_view(), name='editar_equipo'),
+    path('busqueda_equipo/', views.BusquedaEquipo.as_view(), name='busqueda_equipo'),
+    path('lista/', views.ListEquiposPdf.as_view(), name='lista'),
 
 ]
--- inventarioKK/equipo/views.py
+++ inventarioKK/equipo/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from .models import Equipo
-from django.views import generic
+from .models import Equipo, Edificio
+from django.views import generic, View
 from .forms import EquipoForm
 from django.urls import reverse_lazy
+from django.db.models import Q
+from django.http import HttpResponse
+from .utils import render_to_pdf
 
 # Create your views here.
 class ListaDeEquipos(generic.ListView):
     """vista generica tabla equipos"""
+    paginate_by = 7
     template_name= 'equipo/index.html'
     context_object_name = 'lista_equipo'
     model = Equipo
@@ -30,3 +34,27 @@ class EquipoUpdate(generic.UpdateView):
     form_class = EquipoForm
     template_name = 'equipo/nuevo_equipo.html'
     success_url = reverse_lazy('equipo:lista_de_equipos')
+
+class BusquedaEquipo(generic.ListView):
+    """Busca un Equipo"""
+    model = Equipo
+    context_object_name = 'busqueda_equipo'
+    template_name = 'equipo/busqueda_equipo.html'
+
+    def get_queryset(self): # new
+        query = self.request.GET.get('q')
+        object_list = Equipo.objects.filter(
+           Q(id_equipo__icontains=query) | Q(id_tipo_equipo__nombre_tipo_equipo__icontains=query) | Q(id_marca__nombre_marca__icontains=query)| 
+           Q(id_estado_equipo__nombre_estado_equipo__icontains=query) | Q(descripcion__icontains=query) | Q(id_ubicacion__nombre_ubicacion__icontains=query) | Q(id_ubicacion__id_edificio__nombre_edificio__icontains=query) 
+        )
+        return object_list
+
+class ListEquiposPdf(View):
+
+    def get(self, request, *args, **kwargs):
+        descripcion_equipo = Equipo.objects.get(pk=1)
+        data = {
+            'descripcion_equipo': descripcion_equipo
+        }
+        pdf = render_to_pdf('equipo/lista.html', data)
+        return HttpResponse(pdf, content_type='application/pdf')
